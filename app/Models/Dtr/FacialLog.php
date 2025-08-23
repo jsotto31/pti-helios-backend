cbcdbd7ef60805f7f427fa3eaad219516d5d50e4
maxconsul17 <mconsul@schools.ph>
2025-08-23 11:29:51 +0800

    clean process facial log job

--- app/Models/Dtr/FacialLog.php
+++ app/Models/Dtr/FacialLog.php
@@ -10,4 +10,21 @@ class FacialLog extends Model
     use HasFactory;
 
     protected $fillable = ['employee_id', 'time', 'site', 'device_id', 'processed'];
+
+    public function scopeUnprocessedLogs($query)
+    {
+        return $query->select('employee_id')
+                ->where('processed', 0)
+                ->groupBy('employee_id')
+                ->havingRaw('COUNT(*) >= 2')
+                ->pluck('employee_id');
+    }
+
+    public function scopeForEmployeeUnprocessedLogs($query, $employeeId)
+    {
+        return $query->where('employee_id', $employeeId)
+                    ->where('processed', 0)
+                    ->orderBy('time')
+                    ->limit(2);
+    }
 }
